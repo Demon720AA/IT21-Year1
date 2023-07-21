b96a41e8107e745d2b9aec58fc6f715eba60a513
Supanut Arun <139570148+Demon720AA@users.noreply.github.com>
2023-07-21 14:05:58 +0700

    Add files via upload

--- Python/SurprisingVote.py
+++ Python/SurprisingVote.py
@@ -4,5 +4,8 @@ def main():
     per3 = float(input())
     max_per = float(input())
     process1 = (per3 - (max_per*2))
-    print(("Not surprising" * (max_per - process1 <= 2)) + ("Surprising" * (max_per - process1 > 2)))
+    if process1 < 0:
+        process1 = 0
+    print(("Not surprising" * (max_per - process1 <= 2)) + \
+    ("Surprising" * (max_per - process1 > 2)))
 main()
