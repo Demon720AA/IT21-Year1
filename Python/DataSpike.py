3c56984fc7a0d978e132da1b8788b71030b51a25
Supanut Arun <139570148+Demon720AA@users.noreply.github.com>
2023-08-02 20:03:35 +0700

    Add files via upload

---
+++ Python/DataSpike.py
@@ -0,0 +1,30 @@
+'''DataSpike'''
+def main():
+    '''Fine Max Number'''
+    compare_num = 0
+    num1 = int(input())
+    if num1 > compare_num:
+        compare_num = num1
+    num2 = int(input())
+    if num2 > compare_num:
+        compare_num = num2
+    num3 = int(input())
+    if num3 > compare_num:
+        compare_num = num3
+    num4 = int(input())
+    if num4 > compare_num:
+        compare_num = num4
+    num5 = int(input())
+    if num5 > compare_num:
+        compare_num = num5
+    num6 = int(input())
+    if num6 > compare_num:
+        compare_num = num6
+    num7 = int(input())
+    if num7 > compare_num:
+        compare_num = num7
+    num8 = int(input())
+    if num8 > compare_num:
+        compare_num = num8
+    print(compare_num)
+main()
---
+++ Python/FoodGrade I.py
@@ -0,0 +1,37 @@
+'''FoodGrade I'''
+def process1(check):
+    '''input + หาเลขในช่วงที่กำหนด'''
+    chiken = float(input())
+    if 50 <= chiken and chiken <= 70:
+        check = check + 1
+    return check
+
+def fgi():
+    '''หาเลขตามข้อกำหนด'''
+    check = 0
+    check = process1(check)
+    check = process1(check)
+    check = process1(check)
+    check = process1(check)
+    check = process1(check)
+    check = process1(check)
+    check = process1(check)
+    check = process1(check)
+    check = process1(check)
+    check = process1(check)
+    check = process1(check)
+    check = process1(check)
+    check = process1(check)
+    check = process1(check)
+    check = process1(check)
+    check = process1(check)
+    check = process1(check)
+    check = process1(check)
+    check = process1(check)
+    check = process1(check)
+    check = process1(check)
+    check = process1(check)
+    check = process1(check)
+    check = process1(check)
+    print(check)
+fgi()
