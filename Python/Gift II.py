6b344ffc5b0751342b158323bc81184aabda6cbb
Supanut Arun <139570148+Demon720AA@users.noreply.github.com>
2023-08-02 19:17:58 +0700

    Gift II edit Ver.2

--- Python/Gift II.py
+++ Python/Gift II.py
@@ -1,10 +1,39 @@
 '''Gift II'''
 def main():
     '''หาเลขคู่'''
-    list1 = []
-    for _ in range(8):
-        gif = int(input())
-        list1.append(gif)
-    even_nos = [num for num in list1 if num % 2 == 0]
-    print(even_nos[0])
+    gif1 = int(input())
+    gif2 = int(input())
+    gif3 = int(input())
+    gif4 = int(input())
+    gif5 = int(input())
+    gif6 = int(input())
+    gif7 = int(input())
+    gif8 = int(input())
+    if gif1 % 2 == 0:
+        print(gif1)
+    if gif2 % 2 == 0:
+        print(gif2)
+    if gif3 % 2 == 0:
+        print(gif3)
+    if gif4 % 2 == 0:
+        print(gif4)
+    if gif5 % 2 == 0:
+        print(gif5)
+    if gif6 % 2 == 0:
+        print(gif6)
+    if gif7 % 2 == 0:
+        print(gif7)
+    if gif8 % 2 == 0:
+        print(gif8)
 main()
+
+# '''Gift II'''
+# def main():
+#     '''หาเลขคู่'''
+#     list1 = []
+#     for _ in range(8):
+#         gif = int(input())
+#         list1.append(gif)
+#     even_nos = [num for num in list1 if num % 2 == 0]
+#     print(even_nos[0])
+# main()
