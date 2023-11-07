774e97490538ca7ea9a0a5988e8bac4b5354bcff
Supanut Arun <139570148+Demon720AA@users.noreply.github.com>
2023-11-07 22:03:57 +0700

    Final

---
+++ Python/DigitV3.py
@@ -0,0 +1,13 @@
+'''DigitV3'''
+def main():
+    """ Main function """
+    word = input()
+    if "thousand" in word:
+        print(4)
+    elif "hundred" in word:
+        print(3)
+    elif "ty" in word or "teen" in word or "ten" in word or "eleven" in word or "twelve" in word:
+        print(2)
+    else:
+        print(1)
+main()
---
+++ Python/[Recommended Final]PH.py
@@ -0,0 +1,11 @@
+'''PH'''
+def main():
+    '''PH'''
+    num = float(input())
+    if num < 7:
+        print("acidic")
+    elif num > 7:
+        print("akaline")
+    else:
+        print("neutral")
+main()
