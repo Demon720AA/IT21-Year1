d8008783349c2d7b0885fe9e13bf2fa9589eea03
Supanut Arun <139570148+Demon720AA@users.noreply.github.com>
2024-03-15 08:43:23 +0700

    edit

--- src/Lab12/Student.java
+++ src/Lab12/Student.java
@@ -1,7 +1,20 @@
-public class Student {
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Lab12;
+
+/**
+ *
+ * @author pc
+ */
+import java.io.*;
+
+public class Student implements Serializable{
     private String name;
     private int ID;
     private int money;
+    
     public Student(){
         this("", 0, 0);
     }
@@ -13,17 +26,17 @@ public class Student {
     public void setName(String name){
         this.name = name;
     }
-    public void setID(int ID){
-        this.ID = ID;
-    }
-    public void setName(int money){
-        this.money = money;
-    }
     public String getName(){
         return name;
     }
+    public void setID(int ID){
+        this.ID = ID;
+    }
     public int getID(){
         return ID;
+    }
+    public void setMoney(int money){
+        this.money = money;
     }
     public int getMoney(){
         return money;
